2d8ee7b9a7ccb621ed154594a56ef0371e52f2ff
DzakyFirdaus96 <110155168+DzakyFirdaus96@users.noreply.github.com>
2022-12-01 18:53:10 +0700

    Add files via upload

--- sql/Form1.Designer.cs
+++ sql/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace WindowsFormsApplication3
+﻿
+namespace aplikasi_hotel
 {
     partial class Form1
     {
@@ -28,98 +29,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tusername = new System.Windows.Forms.TextBox();
-            this.tPaswd = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // tusername
-            // 
-            this.tusername.Location = new System.Drawing.Point(234, 122);
-            this.tusername.Name = "tusername";
-            this.tusername.Size = new System.Drawing.Size(338, 20);
-            this.tusername.TabIndex = 0;
-            this.tusername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // tPaswd
-            // 
-            this.tPaswd.Location = new System.Drawing.Point(234, 209);
-            this.tPaswd.Name = "tPaswd";
-            this.tPaswd.PasswordChar = '*';
-            this.tPaswd.Size = new System.Drawing.Size(338, 20);
-            this.tPaswd.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(173, 122);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Username";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(173, 212);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Password";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // button1
-            // 
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.button1.Location = new System.Drawing.Point(176, 369);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Log in";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(497, 369);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 447);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tPaswd);
-            this.Controls.Add(this.tusername);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tusername;
-        private System.Windows.Forms.TextBox tPaswd;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- sql/Form1.cs
+++ sql/Form1.cs
@@ -8,64 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication3
+namespace aplikasi_hotel
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (!(tusername.Text == string.Empty) && !(tPaswd.Text == string.Empty))
-            {
-                if ((tusername.Text == "Dzaky") && (tPaswd.Text == "Dzaky"))
-                {
-                  //  MessageBox.Show("Login berhasil", "login page");
-                    Form2 f2 = new Form2();
-                    f2.ShowDialog();
-                }
-                else
-                {
-                    MessageBox.Show("username atau pasword salah", "login page");
-                }
-            }
-            else
-            {
-                MessageBox.Show(" username dan pasword harus di isi", "login page");
-            }
-        }
-
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-
         }
     }
 }
